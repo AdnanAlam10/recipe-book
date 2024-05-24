a640385c648bda4d2dcdc02682b59c88bc382625
Adnan Alam <adnanalamcie@gmail.com>
2024-05-24 17:22:14 +1000

    deployment info

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: "/recipe-book",
+  output: "export", // <=== enables static exports
+  reactStrictMode: true,
+};
 
 export default nextConfig;
